d64e4e0c6e6638dd04e2494d365aff715b7a952d
MarkFilip <4markfilipkowski@gmail.com>
2020-03-23 13:48:11 -0600

    Add Basic HTML and JavaScript 1
    Add both HTML and JS 1

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,15 @@
+var Noise="Hello, World!";
+
+window.alert(Noise);
+
+var X = "The instructor said, \"This here students, is what I call a string.\"";
+var Y = " They responded with: \"Did you have to use a \\ for that.\"";
+var Z = X+Y;
+document.write(Z);
+
+var team = "Greil's mercenaries" , leader = "Ike", support = "Titania", tank = "Gatrie" , range = "Shinnon";
+var team = team.fontcolor("brown") , leader = leader.fontcolor("red"), support = support.fontcolor("green"), tank = tank.fontcolor("blue") , range = range.fontcolor("orange");
+document.write(tank + " "+team)
+
+var exp = 3+3;
+document.write(exp);
